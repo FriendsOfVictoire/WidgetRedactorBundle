3fb8843daca9e892e034cff0683dc26fac9236f9
Thomas Beaujean <thomas@appventus.com>
2014-05-28 14:44:31 +0200

    [VICMS-184] Widget: gestion des modes static, entity et query

--- Widget/Manager/WidgetRedactorManager.php
+++ Widget/Manager/WidgetRedactorManager.php
@@ -4,51 +4,72 @@ namespace Victoire\RedactorBundle\Widget\Manager;
 use Victoire\RedactorBundle\Form\WidgetRedactorType;
 use Victoire\RedactorBundle\Entity\WidgetRedactor;
 use Victoire\Bundle\CoreBundle\Widget\Managers\ManagerInterface;
+use Victoire\Bundle\PageBundle\Entity\BasePage;
+use Victoire\Bundle\CoreBundle\Widget\Managers\BaseWidgetManager;
+use Victoire\Bundle\CoreBundle\Entity\Widget;
+use Victoire\Bundle\CoreBundle\Event\WidgetBuildFormEvent;
+use Victoire\Bundle\CoreBundle\VictoireCmsEvents;
 
 /**
  * CRUD operations on WidgetRedactor Widget
  */
-class WidgetRedactorManager implements ManagerInterface
+class WidgetRedactorManager extends BaseWidgetManager
 {
-protected $container;
-
     /**
-     * constructor
+     * Get a new widget entity
      *
-     * @param ServiceContainer $container
+     * @return Widget
      */
-    public function __construct($container)
-    {
-        $this->container = $container;
-    }
-
-    /**
-     * create a new WidgetRedactor
-     * @param Page   $page
-     * @param string $slot
-     *
-     * @return $widget
-     */
-    public function newWidget($page, $slot)
+    protected function getNewWidgetEntity()
     {
         $widget = new WidgetRedactor();
-        $widget->setPage($page);
-        $widget->setSlot($slot);
 
         return $widget;
     }
+
     /**
      * render the WidgetRedactor
-     * @param Widget $widget
+     * @param WidgetRedactor $widget
      *
      * @return widget show
      */
-    public function render($widget)
+    public function render(Widget $widget)
     {
-        return $this->container->get('victoire_templating')->render(
+        //the templating service
+        $templating = $this->container->get('victoire_templating');
+
+        //the mode of display of the widget
+        $mode = $widget->getMode();
+
+        //the widget must have a mode
+        if ($mode === null) {
+            throw new \Exception('The widget ['.$widget->getId().'] has no mode.');
+        }
+
+        //the content of the widget
+        $content = '';
+
+        switch ($mode) {
+        	case WidgetRedactor::MODE_STATIC:
+        	    $content = $widget->getContent();
+        	    break;
+        	case WidgetRedactor::MODE_ENTITY:
+        	    //get the content of the widget with its entity
+        	    $content = $this->getWidgetEntityContent($widget);
+        	    break;
+        	case WidgetRedactor::MODE_QUERY:
+        	    throw new \Exception('The mode ['.$mode.'] is not yet supported by the widget redactor manager.');
+        	    break;
+        	default:
+        	    throw new \Exception('The mode ['.$mode.'] is not supported by the widget redactor manager.');
+        }
+
+
+        return $templating->render(
             "VictoireRedactorBundle::show.html.twig",
             array(
-                "widget" => $widget
+                "widget" => $widget,
+                "content" => $content
             )
         );
     }
@@ -82,7 +103,17 @@ protected $container;
      */
     public function buildForm($widget, $entityName = null, $namespace = null)
     {
+        //test parameters
+        if ($entityName !== null) {
+            if ($namespace === null) {
+                throw new \Exception('The namespace is mandatory if the entityName is given');
+            }
+        }
+
         $form = $this->container->get('form.factory')->create(new WidgetRedactorType($entityName, $namespace), $widget);
+
+        $dispatcher = $this->container->get('event_dispatcher');
+        $dispatcher->dispatch(VictoireCmsEvents::WIDGET_BUILD_FORM, new WidgetBuildFormEvent($widget, $form));
 
         return $form;
     }
@@ -99,7 +130,6 @@ protected $container;
      */
     public function renderNewForm($form, $widget, $slot, $page, $entity = null)
     {
-
         return $this->container->get('victoire_templating')->render(
             "VictoireRedactorBundle::new.html.twig",
             array(
@@ -118,4 +148,39 @@ protected $container;
         return 'redactor';
     }
 
+    /**
+     * Get the content of the widget by the entity linked to it
+     *
+     * @param Widget $widget
+     *
+     * @return string
+     */
+    protected function getWidgetEntityContent(Widget $widget)
+    {
+        //the result
+        $content = '';
+
+        $entity = $widget->getEntity();
+
+        if ($entity === null) {
+            throw new \Exception('The widget ['.$widget->getId().'] has no entity to display.');
+        }
+
+        $fields = $widget->getFields();
+
+        //test that the widget has some fields
+        if (count($fields) === 0) {
+            throw new \Exception('The widget ['.$widget->getId().'] has no field to display.');
+        }
+
+        //parse the field
+        foreach ($fields as $field) {
+            //get the value of the field
+            $attributeValue =  $this->getEntityAttributeValue($entity, $field);
+            //concantene values
+            $content .= $attributeValue;
+        }
+
+        return $content;
+    }
 }
