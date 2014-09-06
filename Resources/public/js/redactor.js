eebef86940b92beda6f47260e12bee1217fc7553
lenybernard <leny@appventus.com>
2014-09-06 15:55:02 +0200

    fix redactor min-height

--- Resources/public/js/redactor.js
+++ Resources/public/js/redactor.js
@@ -1,11 +1,12 @@
 
-function initRedactor(item_id, buttons, plugins, lang, autoresize){
+function initRedactor(item_id, buttons, plugins, lang, autoresize, minHeight){
     //Default values
     buttons     = typeof buttons !== 'undefined' ? buttons : ['html', '|', 'formatting', '|', 'bold', 'italic', 'deleted', '|', 'unorderedlist', 'orderedlist', 'outdent', 'indent', '|', 'image', 'video', 'file', 'table', 'link', '|', '|', 'alignment', '|', 'horizontalrule'];
     // plugins     = typeof plugins !== 'undefined' ? plugins : ['media', 'clips', 'fontcolor', 'fontfamily', 'fontsize', 'fullscreen'];
     plugins     = typeof plugins !== 'undefined' ? plugins : ['media', 'clips', 'fontcolor', 'fontfamily', 'fontsize', 'fullscreen'];
     lang        = typeof lang !== 'undefined' ? lang : 'fr';
     autoresize  = typeof autoresize !== 'undefined' ? autoresize : true;
+    minHeight  = typeof minHeight !== 'undefined' ? minHeight : 500;
 
     $vic(item_id).redactor({
         lang: lang,
@@ -14,6 +15,7 @@ function initRedactor(item_id, buttons, plugins, lang, autoresize){
         plugins: plugins,
         convertDivs: false,
         paragraphy: false,
+        minHeight: minHeight, // pixels
         buttonsCustom: {
             image: {
                 title: 'Image',
